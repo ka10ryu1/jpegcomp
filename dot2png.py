5e00f189a74e2a4e8d6462ba2835b38fc55eefc3
Ryuichi Kato <lsyp.tuiovjo.1231@gmail.com>
2018-01-17 13:38:48 +0900

    add dot2png.py

---
+++ dot2png.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+help = 'dot言語で記述されたファイルをPNG形式に変換する'
+
+import pydot
+import os
+import argparse
+
+from func import argsPrint
+
+
+def command():
+    parser = argparse.ArgumentParser(description=help)
+    parser.add_argument('dot', nargs='+',
+                        help='使用するdotファイルのパス')
+    parser.add_argument('-e', '--ext', default='png',
+                        help='保存する拡張子 (default: png, other: pdf, svg)')
+    parser.add_argument('-o', '--out_path', default='./result/',
+                        help='出力するフォルダ (default: ./result/)')
+    return parser.parse_args()
+
+
+def main(args):
+    if not os.path.isdir(args.out_path):
+        os.makedirs(args.out_path)
+
+    for name in args.dot:
+        (graph,) = pydot.graph_from_dot_file(name)
+        name, _ = os.path.splitext(os.path.basename(name))
+        if(args.ext == 'png'):
+            graph.write_png(os.path.join(args.out_path, name + '.png'))
+        elif(args.ext == 'pdf'):
+            graph.write_pdf(os.path.join(args.out_path, name + '.pdf'))
+        elif(args.ext == 'svg'):
+            graph.write_svg(os.path.join(args.out_path, name + '.svg'))
+        else:
+            print('[ERROR] ext option miss:', args.ext)
+
+
+if __name__ == '__main__':
+    args = command()
+    argsPrint(args)
+    main(args)
