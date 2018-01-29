5d7023563585b973f9ac6c739cf2c2118e60075c
Ryuichi Kato <lsyp.tuiovjo.1231@gmail.com>
2018-01-29 17:13:50 +0900

    add comment and code refactoring

--- predict_some_snapshot.py
+++ predict_some_snapshot.py
@@ -43,45 +43,83 @@ def command():
     return parser.parse_args()
 
 
-def main(args):
+def getSnapshotAndParam(folder):
+    """
+    入力フォルダからスナップショットとモデルパラメータのパスを取得する
+    スナップショットは作成された日時順にソートする
+    [in]  folder:        スナップショットとモデルパラメータのあるフォルダ
+    [out] snapshot_path: スナップショットのパスのリスト
+    [out] param:         モデルパラメータのパス
+    """
+
     snapshot_path = []
-    param = ''
-    for f in os.listdir(args.snapshot_and_json):
+    param_path = ''
+    for f in os.listdir(folder):
         name, ext = os.path.splitext(os.path.basename(f))
-        full_path = os.path.join(args.snapshot_and_json, f)
-        print(f, name, ext)
+        full_path = os.path.join(folder, f)
+        #print(f, name, ext)
         if('.snapshot' in ext):
             snapshot_path.append(full_path)
         elif('.json' in ext):
-            param = full_path
+            param_path = full_path
 
     snapshot_path = sorted([x for x in snapshot_path
                             if os.path.isfile(x)], key=os.path.getmtime)
-    # jsonファイルから学習モデルのパラメータを取得する
-    unit, ch, layer, af1, af2 = getModelParam(param)
-    # 学習モデルの出力画像のチャンネルに応じて画像を読み込む
+
+    return snapshot_path, param_path
+
+
+def getImage(jpg_path, ch, seed):
+    """
+    画像を読み込んでランダムに取得して連結する
+    [in]  jpg_path: 入力画像のパス
+    [in]  ch:       画像を読み込むチャンネル数
+    [in]  seed:     乱数シード
+    [out] 連結された画像（縦長）
+    """
+
     ch_flg = IMG.getCh(ch)
-    imgs = [cv2.imread(jpg, ch_flg) for jpg in args.jpeg if isImage(jpg)]
+    imgs = [cv2.imread(jpg, ch_flg) for jpg in jpg_path if isImage(jpg)]
     imgs, size = IMG.split(imgs, args.img_size)
     imgs = np.array(IMG.whiteCheck(imgs))
     if(args.random_seed >= 0):
-        np.random.seed(args.random_seed)
+        np.random.seed(seed)
 
     shuffle = np.random.permutation(range(len(imgs)))
-    img = np.vstack(imgs[shuffle[:args.image_num]])
+    return np.vstack(imgs[shuffle[:args.image_num]])
 
+
+def stackImages(imgs, resize):
+    """
+    推論実行で得られた 画像のリストを結合してサイズを調整する
+    [in]  imgs:    入力画像リスト
+    [in]  resize: リサイズする倍率
+    [out] 結合画像
+    """
+
+    img = np.hstack(imgs)
+    w, h = img.shape[:2]
+    return cv2.resize(
+        img,
+        (h * args.img_rate, w * args.img_rate),
+        cv2.INTER_NEAREST
+    )
+
+
+def main(args):
+    # スナップショットとモデルパラメータのパスを取得する
+    snapshot_path, param = getSnapshotAndParam(args.snapshot_and_json)
+    # jsonファイルから学習モデルのパラメータを取得する
+    unit, ch, layer, af1, af2 = getModelParam(param)
+    # 推論実行するために画像を読み込んで結合する
+    img = getImage(args.jpeg, ch, args.random_seed)
     # 学習モデルを生成する
     model = L.Classifier(JC(
-        n_unit=unit,
-        n_out=ch,
-        layer=layer,
-        actfun_1=af1,
-        actfun_2=af2
+        n_unit=unit, n_out=ch, layer=layer,
+        actfun_1=af1, actfun_2=af2
     ))
-
     out_imgs = [img]
     for s in snapshot_path:
-
         # load_npzのpath情報を取得する
         load_path = checkModelType(s)
         # 学習済みモデルの読み込み
@@ -101,13 +139,12 @@ def main(args):
         # 学習モデルを入力画像ごとに実行する
         out_imgs.append(predict(model, args, img, ch, -1))
 
+    # 推論実行した各画像を結合してサイズを調整する
+    img = stackImages(out_imgs, args.img_rate)
     # 生成結果の表示
-    img = np.hstack(out_imgs)
-    w, h = img.shape[:2]
-    img = cv2.resize(img, (h * args.img_rate, w * args.img_rate),
-                     cv2.INTER_NEAREST)
     cv2.imshow('predict some snapshots', img)
     cv2.waitKey()
+    # 生成結果の保存
     cv2.imwrite(F.getFilePath(args.out_path, 'snapshots.jpg'), img)
 
 
