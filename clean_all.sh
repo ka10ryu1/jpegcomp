e97f422eff3a205b7aaab8505aa38228a00da711
Ryuichi Kato <lsyp.tuiovjo.1231@gmail.com>
2018-01-29 14:08:35 +0900

    path update

--- clean_all.sh
+++ clean_all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #clean_all.sh
 rm -rf ./*.npz ./result ./__pycache__/ ./*~ ./log.txt
-rm -rf ./Tools/__pycache__/ ./Tool/*~ ./Tool/result
+rm -rf ./Tools/__pycache__/ ./Tools/*~ ./Tools/result
 rm -rf ./Lib/__pycache__/ ./Lib/*~ ./Lib/result
