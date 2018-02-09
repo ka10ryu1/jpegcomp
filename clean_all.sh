174466235291a7599b7779d85d22c48cae975be9
Ryuichi Kato <lsyp.tuiovjo.1231@gmail.com>
2018-02-09 16:51:02 +0900

    file delete

--- clean_all.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#clean_all.sh
-rm -rf ./*.npz ./result ./__pycache__/ ./*~ ./log.txt
-rm -rf ./Tool/__pycache__/ ./Tool/*~ ./Tool/result
-rm -rf ./Lib/__pycache__/ ./Lib/*~ ./Lib/result
